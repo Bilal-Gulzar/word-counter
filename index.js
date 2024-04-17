a7593e317ba4f0a2417cc40290d590b9ceb6f716
Bilal-gulzar <bilalgul415@gamil.com>
2024-04-17 17:00:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// This is word counter code.
+const Para = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Write your sentence and count your sentence words:"
+});
+const wordCounter = Para.sentence.trim().split(" ");
+console.log(wordCounter);
+console.log(`your sentence word count is ${wordCounter.length}`);
